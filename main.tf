8ed72c5b68469361bed685c0b8f08c1cfcc6280c
Simon-Shlomo Poil <simon-shlomo@users.noreply.github.com>
2023-01-08 13:19:48 +0100

    Initial commit (#1)

---
+++ main.tf
@@ -0,0 +1,44 @@
+# Configure GCP project
+provider "google" {
+  project = "rare-highway-373710"
+}
+
+# Deploy image to Cloud Run
+resource "google_cloud_run_service" "mywebapp" {
+  name     = "mywebapp"
+  location = "us-central1"
+  template {
+    spec {
+      containers {
+        image = "gcr.io/rare-highway-373710/webapp"
+      }
+    }
+  }
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
+
+# Create public access
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+# Enable public access on Cloud Run service
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location    = google_cloud_run_service.mywebapp.location
+  project     = google_cloud_run_service.mywebapp.project
+  service     = google_cloud_run_service.mywebapp.name
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
+
+# Return service URL
+output "url" {
+  value = "${google_cloud_run_service.mywebapp.status[0].url}"
+}
